abfdfcd6d6a53ce68f34f8b7e171147a5b3d8397
Frank Santos <fransecre@gmail.com>
2023-04-10 16:25:32 +0200

    New Project

--- demoFormulario/src/main.jsx
+++ demoFormulario/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import DemoComponent from './components/DemoComponent'
+import Header from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DemoComponent />
-    <DemoComponent />
+    <Header />
   </React.StrictMode>,
 )
