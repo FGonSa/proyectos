17cbc7e822742acbbe82687d1ce9da75a5c61d3b
Francisco Javier González <fransecre@gmail.com>
2023-04-18 23:07:57 +0200

    setCategoríaActual working

--- app-kiosko/src/components/Categoria.jsx
+++ app-kiosko/src/components/Categoria.jsx
@@ -1,3 +1,4 @@
+import useKiosko from '@/hooks/useKiosko';
 import Image from 'next/image';
 import React from 'react'
 
@@ -5,13 +6,21 @@ function Categoria({categoria}) {
 
     const {name, icon, id} = categoria;
 
+    const {categoriaActual, handleClickCategoria} = useKiosko()
+
   return (
-    <div className='flex items-center gap-4 w-full border p-5 hover:bg-amber-300'>
+    //Si la categoría actual coincide con el id recibido, se queda seleccionada
+    //El ? es porque al iniciar por primera vez la app puede no haber ID
+    <div className={`${
+      categoriaActual?.id === id ? "bg-amber-300"  : ""}
+    flex items-center gap-4 w-full border p-5 hover:bg-amber-300`}>
         <Image 
         src={`/assets/img/icon/${icon}`} width={70} height={70} alt="Icono Categoría" 
         />
 
-        <button type='button' className='text-2xl font-bold hover:cursor-pointer'>
+        <button 
+        onClick={() => handleClickCategoria(id)}
+        type='button' className='text-2xl font-bold hover:cursor-pointer'>
             {name}
         </button>
     </div>
--- app-kiosko/src/context/KioskoProvider.jsx
+++ app-kiosko/src/context/KioskoProvider.jsx
@@ -1,35 +1,46 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
+const KioskoContext = createContext();
 
-const KioskoContext = createContext()
+const KioskoProvider = ({ children }) => {
+  //Array vacío
+  const [categorias, setCategorias] = useState([]);
 
-const KioskoProvider = ({children}) => {
+  const [categoriaActual, setCategoriaActual] = useState({});
 
-    //Array vacío
-    const [categorias, setCategorias] = useState([])
+  //Obtenemos las categorías de la Base de Datos mediante Axios
+  //Guardamos las categorías en el array inicial vacío
+  const obtenerCategorias = async () => {
+    const { data } = await axios("/api/categorias");
+    setCategorias(data);
+  };
 
-    //Obtenemos las categorías de la Base de Datos mediante Axios
-    //Guardamos las categorías en el array inicial vacío
-    const obtenerCategorias = async () => {
-        const {data} = await axios('/api/categorias')
-        setCategorias(data)
-    }
+  //
+  useEffect(() => {
+    obtenerCategorias();
+  }, []);
 
-    //
-    useEffect(()=>{
-        obtenerCategorias()
-    }, [])
+  //Recibe un el id del objeto clickado
+  //Filtramos las categorías por el id recibido
+  //Al encontrar la categoría con el id recibido, nos devuelve un array con esa categoría
+  //Como sólo devuelve 1 posición, la categoría actual pasa a ser esa posición
+  const handleClickCategoria = (id) => {
+    
+    const categoria = categorias.filter((cat) => cat.id === id);
+    console.log(categoria)
+    setCategoriaActual(categoria[0]);
+  };
 
-    return(
-        <KioskoContext.Provider
-        value={{ categorias }}
-        >
-            {children}
-        </KioskoContext.Provider>
-    )
-}
+  return (
+    <KioskoContext.Provider
+      value={{ categorias, categoriaActual, handleClickCategoria }}
+    >
+      {children}
+    </KioskoContext.Provider>
+  );
+};
 
-export {KioskoProvider}
+export { KioskoProvider };
 
-export default KioskoContext
+export default KioskoContext;
